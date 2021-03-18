9065a47feeca11d04b00d0f67c45f54b62209d41
DomVega <domenicovega@knights.ucf.edu>
2021-03-18 03:40:20 -0400

    It's alive, it's moving... IT'S ALIVE!
    Created NavMesh and guard movement/pathfinding. Guards move from point to point with a chance of waiting at next point.

--- Project 2 - 3D Exploration/Assets/Scripts/EnemyController.cs
+++ Project 2 - 3D Exploration/Assets/Scripts/EnemyController.cs
@@ -1,32 +1,126 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
-    
-    public Transform startMarker;
-    public Transform endMarker;
+    //Decides whether the agent waits on each node
+    [SerializeField]
+    bool patrolWaiting;
+    //The total time waited at node 
+    [SerializeField]
+    float totalWaitTime = 3f;
+    //
+    [SerializeField]
+    float waitProbability = 0.5f;
+    [SerializeField]
+    float switchProbability = 0.2f;
+    //List of all patrol nodes/points
+    [SerializeField]
+    List<Transform> patrolPoints;
 
-    public float speed = 1.0F;
+    NavMeshAgent navMeshAgent;
+    int currentPatrolIndex;
+    bool traveling;
+    bool waiting;
+    bool patrolForward;
+    float waitTimer;
 
-    private float startTime;
-
-    private float journeyLength;
-
-    void Start()
+    public void Start()
     {
-        startTime = Time.time;
+        navMeshAgent = GetComponent<NavMeshAgent>();
 
-        journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
+        if(navMeshAgent == null)
+        {
+            Debug.Log("The NavMesh is not attached to " + gameObject.name);
+        }
+        else
+        {
+            if(patrolPoints != null && patrolPoints.Count >= 2)
+            {
+                currentPatrolIndex = 0;
+                SetDestination();
+            }
+            else
+            {
+                Debug.Log("There are not enough patrol points to patrol with");
+            }
+        }
+    }
+    //Checks if close to destination 
+    public void Update()
+    {
+       if(traveling && navMeshAgent.remainingDistance <= 1.0f)
+       {
+           traveling = false;
+           //if is going to wait then waits
+           if(patrolWaiting)
+           {
+               waiting = true;
+               waitTimer = 0f;
+           }
+           else
+           {
+               ChangePatrolPoint();
+               SetDestination();
+           }
+       }
+       //If we are waiting increment wait timer
+       if(waiting)
+       {
+           waitTimer += Time.deltaTime;
+           if (waitTimer >= totalWaitTime)
+           {
+               waiting = false;
+               patrolWaiting = false;
+
+               ChangePatrolPoint();
+               SetDestination();
+           }
+       }
     }
 
-    void FixedUpdate()
+    private void SetDestination()
     {
-        float distCovered = (Time.time - startTime) * speed;
+        if(patrolPoints != null)
+        {
+            Vector3 targetVector = patrolPoints[currentPatrolIndex].transform.position;
+            navMeshAgent.SetDestination(targetVector);
+            traveling = true;
+        }
+    }
+    
+    //Selects a new patrol point in the available list but also with a small probability of moving forwards or backwards
+    private void ChangePatrolPoint()
+    {
+        if(UnityEngine.Random.Range(0f, 1f) <= switchProbability)
+        {
+            patrolForward = !patrolForward;
+        }
 
-        float fracJourney = distCovered / journeyLength;
-        
-        transform.position = Vector3.Lerp(startMarker.position, endMarker.position, Mathf.PingPong (fracJourney, 1));
+        if(UnityEngine.Random.Range(0f, 1f) <= waitProbability)
+        {
+            patrolWaiting = true;
+        }
+
+        if(patrolForward)
+        {
+           currentPatrolIndex++;
+
+           if(currentPatrolIndex >= patrolPoints.Count)
+           {
+               currentPatrolIndex = 0;
+           }
+        }
+        else
+        {
+            currentPatrolIndex--; 
+
+            if(currentPatrolIndex < 0)
+            {
+                currentPatrolIndex = patrolPoints.Count - 1;
+            }
+        }
     }
 }

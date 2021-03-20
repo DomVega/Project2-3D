32063e8465aa4b7854eb933df3fb079482bc1320
DomVega <domenicovega@knights.ucf.edu>
2021-03-19 21:52:47 -0400

    Removed Cursor Lock

--- Project 2 - 3D Exploration/Assets/Scripts/MouseController.cs
+++ Project 2 - 3D Exploration/Assets/Scripts/MouseController.cs
@@ -11,7 +11,7 @@ public class MouseController : MonoBehaviour
     float xRotation = 0f;
     void Start()
     {
-       Cursor.lockState = CursorLockMode.Locked;
+       //Cursor.lockState = CursorLockMode.Locked;
     }
  
     void LateUpdate()
@@ -24,12 +24,12 @@ public class MouseController : MonoBehaviour
         transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
         playerBody.Rotate(Vector3.up * mouseX);
 
-        if(Input.GetKeyDown("escape"))
+        /*if(Input.GetKeyDown("escape"))
        {
            Cursor.lockState = CursorLockMode.None;
            Cursor.visible = true;
        }
-
+        */
     }
 
 }
